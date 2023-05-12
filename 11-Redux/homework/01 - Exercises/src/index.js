2ed94a1d90a3cbd4be296cc8e989247f4f43c3b7
cristhianleon22 <cristhiancleon@gmail.com>
2023-05-12 15:02:49 -0500

    11-ok

--- 11-Redux/homework/01 - Exercises/src/index.js
+++ 11-Redux/homework/01 - Exercises/src/index.js
@@ -3,10 +3,10 @@ const contador = require("./reducer");
 const { incremento, decremento } = require("./actions");
 
 // En esta línea creamos nuestro store. Pasándole como parámetro nuestro Reducer
-var store;
+var store= createStore(contador);
 
 // Obtenemos el elemento con el id `valor`.
-var valor;
+var valor = document.getElementById('valor');
 
 // Esta función nos va a servir para actualizar nuestro DOM con el valor que tengamos en nuestro Store.
 // En el primer render y cada vez que nos subscribamos al Store.
@@ -14,11 +14,24 @@ var valor;
 function renderContador() {
   // Obtenemos la propiedad 'contador' de nuestro store:
   // Seteamos el número obtenido como texto dentro del elemento con id 'valor':
+  const estadoActual = store.getState().contador
+  valor.innerHTML =estadoActual;
+
+
 }
 
 // Ejecutamos la función 'renderContador':
-
+renderContador()
 // Nos subscribimos al store pasándole la misma función. Así cada vez que llegue una acción, ejecutamos la función:
-
+store.subscribe(renderContador)
 // Por último, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
 // hagan un dispatch al store de la acción correspondiente:
+const increment = document.querySelector('#incremento')
+const decrement= document.querySelector('#decremento')
+
+increment.addEventListener('click', () => {
+  store.dispatch(incremento())
+})
+decrement.addEventListener('click', () => {
+  store.dispatch(decremento())
+})
--- 11-Redux/homework/01 - Exercises/src/reducer/index.js
+++ 11-Redux/homework/01 - Exercises/src/reducer/index.js
@@ -10,6 +10,27 @@ const initialState = {
 
 function contador(state = initialState, action) {
   
+  switch(action.type){
+
+    case INCREMENTO:
+      return {
+        ...state,
+        contador: state.contador + 1 
+      }
+
+      case DECREMENTO:
+        return{
+          ...state,
+          contador: state.contador - 1
+        }
+
+    default: 
+    return{
+      ...state
+    } 
+
+  }
+
 }
 
 module.exports = contador;
